bb0fb9607e2d1657509617e26f27361b451f2028
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-10-24 11:23:48 -0300

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :cpf, :phone
-
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "invalid email format" }
-  validates :cpf, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/, message: "invalid CPF format" } allow_blank: true
-  validates :phone, format: { with: /\A\(\d{2}\) \d{4,5}-\d{4}\z/, message: "invalid phone format" } allow_blank: true
+  validates :cpf, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/, message: "invalid CPF format" }, allow_blank: true
+  validates :phone, format: { with: /\A\(\d{2}\) \d{4,5}-\d{4}\z/, message: "invalid phone format" }, allow_blank: true
 end
